42bc7653502934741c8b8e9543dbdd125e876b5c
Cytrian <marcus@limette.de>
2017-02-02 04:26:42 +0100

    Change for sonatype nexus 3.2 (#1)
    * Change for sonatype nexus 3.2
    
    * Use NEXUS_HOME instead of NEXUS_BASE

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -eo pipefail
 
-if [ ! -f "$NEXUS_BASE/etc/ssl/keystore.jks" ]; then
-	mkdir -p $NEXUS_BASE/etc/ssl
-	openssl pkcs12 -export -in $PUBLIC_CERTIFICATE -inkey $PRIVATE_KEY -out $NEXUS_BASE/etc/ssl/jetty.key -passout pass:$PRIVATE_KEY_PASSWORD
-	$JAVA_HOME/bin/keytool -importkeystore -noprompt -deststorepass $PRIVATE_KEY_PASSWORD -destkeypass $PRIVATE_KEY_PASSWORD -destkeystore $NEXUS_BASE/etc/ssl/keystore.jks -srckeystore $NEXUS_BASE/etc/ssl/jetty.key -srcstoretype PKCS12 -srcstorepass $PRIVATE_KEY_PASSWORD
-	xsltproc -o $NEXUS_BASE/etc/jetty-https.xml --stringparam password $PRIVATE_KEY_PASSWORD /tmp/jetty-https.xsl $NEXUS_BASE/etc/jetty-https.xml
-	awk 'BEGIN{FS="=";OFS="="} $1 ~ /^nexus-args$/{$2=$2",${karaf.etc}/jetty-https.xml"}{print}' $NEXUS_BASE/etc/org.sonatype.nexus.cfg > /tmp/org.sonatype.nexus.cfg
-	mv /tmp/org.sonatype.nexus.cfg $NEXUS_BASE/etc/org.sonatype.nexus.cfg
-	echo "application-port-ssl=$SSL_PORT" >> $NEXUS_BASE/etc/org.sonatype.nexus.cfg
+if [ ! -f "$NEXUS_HOME/etc/ssl/keystore.jks" ]; then
+	mkdir -p $NEXUS_HOME/etc/ssl
+	openssl pkcs12 -export -in $PUBLIC_CERTIFICATE -inkey $PRIVATE_KEY -out $NEXUS_HOME/etc/ssl/jetty.key -passout pass:$PRIVATE_KEY_PASSWORD
+	$JAVA_HOME/bin/keytool -importkeystore -noprompt -deststorepass $PRIVATE_KEY_PASSWORD -destkeypass $PRIVATE_KEY_PASSWORD -destkeystore $NEXUS_HOME/etc/ssl/keystore.jks -srckeystore $NEXUS_HOME/etc/ssl/jetty.key -srcstoretype PKCS12 -srcstorepass $PRIVATE_KEY_PASSWORD
+	xsltproc -o $NEXUS_HOME/etc/jetty/jetty-https.xml --stringparam password $PRIVATE_KEY_PASSWORD /tmp/jetty-https.xsl $NEXUS_HOME/etc/jetty/jetty-https.xml
+	awk 'BEGIN{FS="=";OFS="="} $1 ~ /^nexus-args$/{$2=$2",${jetty.etc}/jetty-https.xml"}{print}' $NEXUS_HOME/etc/nexus-default.properties > /tmp/nexus-default.properties
+	mv /tmp/nexus-default.properties $NEXUS_HOME/etc/nexus-default.properties
+	echo "application-port-ssl=$SSL_PORT" >> $NEXUS_HOME/etc/nexus-default.properties
 	rm /tmp/*.xsl
 fi
 
-exec $NEXUS_BASE/bin/nexus run
+exec $NEXUS_HOME/bin/nexus run
